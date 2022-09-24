ad16761580313f7e6482bd17ef2bbeaa7b36d0c3
Mike Bennett <mvbennett9@gmail.com>
2022-09-24 23:21:49 +0900

    trying to make exercises a subdocument

--- db.js
+++ db.js
@@ -4,20 +4,20 @@ mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTo
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: {type: String, required: true, dropDups: true}
-});
-
-const User = mongoose.model('User', userSchema);
-
 const exerciseSchema = new Schema({
-  _id: String,
   description: {type: String, required: true},
   duration: {type: Number, required: true},
   date: String
 })
 
 const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+const userSchema = new Schema({
+  username: {type: String, required: true, dropDups: true},
+  log: [exerciseSchema]
+});
+
+const User = mongoose.model('User', userSchema);
 
 const createUser = (username, done) => {
   User.findOne({username: username}, (err, user) => {
